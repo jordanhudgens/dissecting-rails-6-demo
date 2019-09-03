d998a72c544621ddb3e9c7dce9a8c146966204ba
jordanhudgens <jordan@devcamp.com>
2019-09-03 14:36:00 -0700

    Added in the following rules for users in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :guides, dependent: :destroy
+
+  has_many :users_are_following_this_user, class_name:  "Following",
+                                           foreign_key: "followed_id",
+                                           dependent:   :destroy
+
+  has_many :followers, through: :users_are_following_this_user,
+                       source: :follower
+
+  has_many :this_user_is_following_other_users, class_name:  "Following",
+                                                foreign_key: "follower_id",
+                                                dependent:   :destroy
+
+  has_many :following, through: :this_user_is_following_other_users,
+                       source: :followed
 end
