6c29a8bbbd0adb08c8a27bf77eb501a1ea3262a8
jordanhudgens <jordan@devcamp.com>
2019-09-23 15:24:08 -0700

    Created a placeholder profile view and updated the route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :guides
   devise_for :users
-  get 'profile/:slug', to: 'profiles#show'
+  get 'profile/:id', to: 'profiles#show'
   root to: 'static#homepage'
 end
