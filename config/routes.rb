08dd025a4ddb954eb3ec342f447701b8688a143d
jordanhudgens <jordan@devcamp.com>
2019-09-28 11:20:30 -0700

    Created a dedicated search query route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :guides
+  get :search_query, to: 'search#search_query', as: :search_query
   devise_for :users
   get 'profile/:id', to: 'profiles#show'
   root to: 'static#homepage'
