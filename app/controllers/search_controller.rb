dd0fb6e687d0ceda8f47c25db5e18f60eda68d79
jordanhudgens <jordan@devcamp.com>
2019-10-24 10:53:00 -0700

    Integrated pg scope for searching through guides

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,5 @@
 class SearchController < ApplicationController
   def search_query
-    # TODO
+    @guides = Guide.search_by_term(params[:q])
   end
 end
--- app/models/guide.rb
+++ app/models/guide.rb
@@ -2,6 +2,19 @@ class Guide < ApplicationRecord
   has_rich_text :content
   has_one_attached :featured_image
 
+  include PgSearch::Model
+  pg_search_scope :search_by_term,
+    against: [:title, :content],
+    associated_against: {
+      topic: [:title],
+    },
+    using: {
+      tsearch: {
+        any_word: true,
+        prefix: true
+      }
+    }
+
   belongs_to :topic
   belongs_to :user
 
