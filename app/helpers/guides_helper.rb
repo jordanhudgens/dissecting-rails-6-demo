9cd9c0d062aeb620a975998a37333ee9546889fc
jordanhudgens <jordan@devcamp.com>
2019-10-24 10:14:14 -0700

    Created a guide image helper

--- app/helpers/guides_helper.rb
+++ app/helpers/guides_helper.rb
@@ -1,2 +1,9 @@
 module GuidesHelper
+  def guide_thumb guide
+    if guide.featured_image.attached?
+      image_tag(guide.featured_image)
+    else
+      image_tag('placeholders/empty-thumb.png')
+    end
+  end
 end
