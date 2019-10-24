2dd2650b55a0118786f74ff429588b060f6450c9
jordanhudgens <jordan@devcamp.com>
2019-10-24 10:21:36 -0700

    Created a set of topic routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :guides
+  resources :topics
   get :search_query, to: 'search#search_query', as: :search_query
   devise_for :users
   get 'profile/:id', to: 'profiles#show'
