75679653978347108a121c1e8cfede724633dcdc
jordanhudgens <jordan@devcamp.com>
2019-10-24 10:48:19 -0700

    Built out the profile page for users so that they have pagination

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
 class ProfilesController < ApplicationController
   def show
     @user = User.friendly.find(params[:id])
+    @guides = @user.guides.order("created_at DESC").page(params[:page]).per(20)
   end
 end
