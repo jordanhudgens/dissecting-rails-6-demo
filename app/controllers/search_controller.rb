fe9dad8ce9d502e712761dee880b136708b031d4
jordanhudgens <jordan@devcamp.com>
2019-10-24 10:55:32 -0700

    Built out the full functionality for searching

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
 class SearchController < ApplicationController
   def search_query
-    @guides = Guide.search_by_term(params[:q])
+    @query = params[:q]
+    @guides = Guide.search_by_term(@query).page(params[:page]).per(20)
   end
 end
