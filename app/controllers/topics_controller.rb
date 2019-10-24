93e2fefca47c43a32a7af1349f8e5226179b2bdc
jordanhudgens <jordan@devcamp.com>
2019-10-24 10:24:33 -0700

    Built out the initial topic show page, including pagination and rendering nested guides

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,6 @@
 class TopicsController < ApplicationController
   def show
     @topic = Topic.find(params[:id])
+    @guides = @topic.guides.order("created_at DESC").page(params[:page]).per(20)
   end
 end
