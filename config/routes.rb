735193365bd252864176506bbab81aa28fe7ece9
jordanhudgens <jordan@devcamp.com>
2019-10-29 15:43:19 -0600

    Created an index route for profiles

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get :search_query, to: 'search#search_query', as: :search_query
   devise_for :users
   get 'profile/:id', to: 'profiles#show', as: :profile
+  get :profiles, to: 'profiles#index', as: :profiles
   root to: 'static#homepage'
 end
