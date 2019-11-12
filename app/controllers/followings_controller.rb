f8495fff01b8f2aacfc30641b07d7420afb50292
jordanhudgens <jordan@devcamp.com>
2019-11-11 21:14:23 -0700

    Built out the initial following workflow

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -1,5 +1,13 @@
 class FollowingsController < ApplicationController
   def create
-    render json: { params: params.inspect, user: current_user.inspect }
+    following = Following.new
+    following.followed_id = current_user.id
+    following.follower_id = User.find(params[:user_id_to_follow])
+
+    if following.save
+      render json: { following: following, status: 200 }
+    else
+      render json: { errors: following.errors, status: 422 }
+    end
   end
 end
