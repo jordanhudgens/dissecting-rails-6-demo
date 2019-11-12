4c63172f4db2cdcd82eef6cda0e7255c65282cc9
jordanhudgens <jordan@devcamp.com>
2019-11-11 21:20:25 -0700

    Implemented working solution for the following creation functionality

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -1,10 +1,11 @@
 class FollowingsController < ApplicationController
   def create
+    user_to_follow = User.find(params[:user_id_to_follow])
     following = Following.new
     following.followed_id = current_user.id
-    following.follower_id = User.find(params[:user_id_to_follow])
+    following.follower_id = user_to_follow.id
 
-    if following.save
+    if following.save!
       render json: { following: following, status: 200 }
     else
       render json: { errors: following.errors, status: 422 }
