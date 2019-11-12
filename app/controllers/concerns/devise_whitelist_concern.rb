ba0091cdeb4541555fcd2e9597f3e87c433cac6f
jordanhudgens <jordan@devcamp.com>
2019-11-11 21:57:39 -0700

    Created a devise whitelist concern that allows for working with the username

---
+++ app/controllers/concerns/devise_whitelist_concern.rb
@@ -0,0 +1,12 @@
+module DeviseWhitelistConcern
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+end
