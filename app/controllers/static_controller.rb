e5d752805072f1d9959b959b35f04254be91b5c0
jordanhudgens <jordan@devcamp.com>
2019-11-19 12:42:13 -0700

    Styled the homepage feed

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,9 @@
 class StaticController < ApplicationController
   def homepage
-    @guides = UserFeed.new(current_user).call
+    @guides = UserFeed
+                .new(current_user)
+                .call
+                .page(params[:page])
+                .per(20)
   end
 end
