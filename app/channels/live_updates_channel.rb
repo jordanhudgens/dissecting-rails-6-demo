d68b1a23f19feebd2c8225d1856dd3e424257a52
jordanhudgens <jordan@devcamp.com>
2019-11-21 15:59:58 -0700

    Removed debugging

--- app/channels/live_updates_channel.rb
+++ app/channels/live_updates_channel.rb
@@ -1,6 +1,5 @@
 class LiveUpdatesChannel < ApplicationCable::Channel
   def subscribed
-    puts "PARAMS" * 100, params.inspect, "params" * 100
     stream_from "guide_channel_#{params[:id]}"
   end
 end
